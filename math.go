5d01abb4bdaa8e56646cf4f21a66312eafb514c2
abhishektripathi12 <abhishek.glbitm@gmail.com>
2021-09-14 00:58:48 +0530

    third commit

---
+++ math.go
@@ -0,0 +1,13 @@
+package stringutils
+
+func Max(a int, b int) int {
+	if a > b {
+		return 2 * a
+	} else if a == b {
+		return 2 * a
+	} else {
+		return 2 * b
+
+	}
+	//return strings.ToUpper(s)
+}
